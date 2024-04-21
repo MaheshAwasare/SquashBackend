bc1968e40759b804425f647ff3baff9eb825e466
Mahesh Awasare <maheshawasare@gmail.com>
2024-04-21 08:01:59 +0530

    Update

--- Server.js
+++ Server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const db = require('./db'); 
 const app = express();
 const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
     res.send('Hello from Node.js server!');
   });
 
+
+// Endpoint to execute SQL query
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM UserDetails';
   
+    db.query(sql, [], (err, results) => {
+      if (err) {
+        console.error('Error executing query: ' + err.message);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      res.json(results);
+    });
+  });
+
 
   //Start the server
    app.listen(port, () => {
---
+++ db.js
@@ -0,0 +1,36 @@
+const mysql = require('mysql');
+
+// Database configuration
+const dbConfig = {
+  host: 'sql11.freesqldatabase.com',
+  user: 'sql11699613',
+  password: 'wjtA5GqSGz',
+  database: 'sql11699613',
+  port: 3306
+};
+
+// Create a MySQL pool
+const pool = mysql.createPool(dbConfig);
+
+// Function to execute SQL queries
+const query = (sql, params, callback) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to the database: ' + err.message);
+      return callback(err, null);
+    }
+
+    connection.query(sql, params, (err, results) => {
+      connection.release(); // Release the connection
+
+      if (err) {
+        console.error('Error executing query: ' + err.message);
+        return callback(err, null);
+      }
+
+      return callback(null, results);
+    });
+  });
+};
+
+module.exports = { query };
